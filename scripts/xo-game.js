7fd05dff41bf12d680fa961a05a4109049b125a0
Buludkhan <kulmamedoff2015@yandex.ru>
2020-05-12 20:50:46 +0300

    xoGame ChoiceSide

--- scripts/xo-game.js
+++ scripts/xo-game.js
@@ -1,17 +1,24 @@
-let table = document.getElementById('00');
-let x = "X";
-let o = "O";
-table.onclick = function (event) {
+let xoSettings = {
+    height: 3,
+    width: 3,
+    playerSide: "X",
+    botSide: "O",
+}
+let table;
+document.getElementById("buttonX").addEventListener("click", choice);
+document.getElementById("buttonO").addEventListener("click", choice);
+function tableOnClick(event) {
     let target = event.target;
-    if (target.className != "xo" || target.innerHTML != "" || won(x) || won(o)) return;
-    target.innerHTML = x;
+    if (target.className != "xo" || target.innerHTML != "" || won(xoSettings.playerSide) || won(xoSettings.botSide)) return;
+    target.innerHTML = xoSettings.playerSide;
 
-    if (x == won(x)){
-        console.log('x Win');
+
+    if (xoSettings.playerSide == won(xoSettings.playerSide)) {
+        console.log(xoSettings.playerSide + '\tWin');
         return;
     }
 
-    if (draw()){
+    if (draw()) {
         console.log('Draw');
         return;
     }
@@ -19,49 +26,85 @@ table.onclick = function (event) {
     while (true) {
         let x = random(3);
         let y = random(3);
-        let newId = x + "_" + y;
 
-        if (document.getElementById(newId).innerHTML != "") continue;
-        document.getElementById(newId).innerHTML = o;
+        if (table.rows[x].cells[y].innerHTML != "") continue;
+        table.rows[x].cells[y].innerHTML = xoSettings.botSide;
         break;
     }
 
-    if (o == won(o)){
-        console.log('o Win');
+    if (xoSettings.botSide == won(xoSettings.botSide)) {
+        console.log(xoSettings.botSide + '\tWin');
         return;
     }
 
 }
+
 function random(max) {
     return Math.floor(Math.random() * max);
 }
-function won(str){
-    if ( table.rows[0].cells[0].innerHTML == str 
-        && table.rows[1].cells[1].innerHTML == str 
-        && table.rows[2].cells[2].innerHTML == str 
-        || table.rows[2].cells[0].innerHTML == str 
-        && table.rows[1].cells[1].innerHTML == str 
+
+function won(str) {
+    if (table.rows[0].cells[0].innerHTML == str
+        && table.rows[1].cells[1].innerHTML == str
+        && table.rows[2].cells[2].innerHTML == str
+        || table.rows[2].cells[0].innerHTML == str
+        && table.rows[1].cells[1].innerHTML == str
         && table.rows[0].cells[2].innerHTML == str) return str;
-    
-    for (let i = 0; i < 3; i++){
-        if (table.rows[i].cells[0].innerHTML == str 
-        && table.rows[i].cells[1].innerHTML == str 
-        && table.rows[i].cells[2].innerHTML == str 
-        || table.rows[0].cells[i].innerHTML == str 
-        && table.rows[1].cells[i].innerHTML == str 
-        && table.rows[2].cells[i].innerHTML == str) return str;
+
+    for (let i = 0; i < 3; i++) {
+        if (table.rows[i].cells[0].innerHTML == str
+            && table.rows[i].cells[1].innerHTML == str
+            && table.rows[i].cells[2].innerHTML == str
+            || table.rows[0].cells[i].innerHTML == str
+            && table.rows[1].cells[i].innerHTML == str
+            && table.rows[2].cells[i].innerHTML == str) return str;
     }
 }
-function draw(){
-   for (let i = 0; i < 3; i++){
-      for (let j = 0; j < 3; j++){
-          if (table.rows[i].cells[j].innerHTML != ""){
-              continue;
-          }
-          else {
-              return;
-          }
-      } 
-   }
-   return true;
+
+function draw() {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (table.rows[i].cells[j].innerHTML != "") {
+                continue;
+            }
+            else {
+                return;
+            }
+        }
+    }
+    return true;
+}
+
+function choice(event) {
+    table = document.createElement('table');
+    table.addEventListener("click",tableOnClick);
+    for (let i = 0; i < 3; i++) {
+        let tr = document.createElement("tr");
+        table.append(tr);
+
+        for (let j = 0; j < 3; j++) {
+            let td = document.createElement("td");
+            tr.append(td);
+            td.className = "xo";
+        }
+    }
+    table.className = "xo-field";
+    let game = document.querySelector(".xo-game");
+    game.append(table);
+    document.querySelector(".xo-form").style.visibility = "hidden";
+    let target = event.target;
+    if (target.id == "buttonX"){
+        xoSettings.playerSide = "X";
+        xoSettings.botSide = "O";
+    }
+    else if (target.id = "buttonO"){
+        xoSettings.playerSide = "O";
+        xoSettings.botSide = "X";
+        let x = random(3);
+        let y = random(3);
+
+        if (table.rows[x].cells[y].innerHTML != "");
+        table.rows[x].cells[y].innerHTML = xoSettings.botSide;
+    }
+    
 }
